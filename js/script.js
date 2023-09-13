ef16546e8e570cb0ac80a31f4eb60d289ef671a1
ografael <ografael86@gmail.com>
2023-09-13 08:16:13 -0300

    add Book constructor, complement script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,34 @@
 const form = document.getElementById('add-book-form');
+const addBookModal = document.getElementById('add-book-modal');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+let library = []
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let formElements = event.target.elements
+
+    let book = new Book(
+        formElements.title.value,
+        formElements.author.value,
+        formElements.pages.value,
+        formElements.read.checked
+    )
+
+    library.push(book)
+
+    form.reset()
+
+    bootstrap.Modal.getInstance(addBookModal).hide();
 });
+
+
+class Book {
+    constructor(title, author, pages, read = false) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+}
