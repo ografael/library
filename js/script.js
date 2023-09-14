92fb0597ee79697eca799390c70cfc93df480d37
ografael <ografael86@gmail.com>
2023-09-14 14:45:48 -0300

    add remove button, refactoring

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const form = document.getElementById('add-book-form')
 const modal = document.getElementById('add-book-modal')
 const table = document.getElementById('books-table')
 
+const getRandomId = () => { return (Math.random() + 1).toString(36).substring(7) }
+
 let library = []
 
 form.addEventListener('submit', (event) => {
@@ -21,29 +23,6 @@ form.addEventListener('submit', (event) => {
     form.reset()
 })
 
-const build = (elements) => {
-    return new Book(
-        elements.title.value,
-        elements.author.value,
-        elements.pages.value,
-        elements.is_read.checked
-    )
-}
-
-const add = (book) => {
-    var row = table.insertRow()
-
-    var title = row.insertCell(0)
-    var author = row.insertCell(1)
-    var pages = row.insertCell(2)
-    var isRead = row.insertCell(3)
-
-    title.innerHTML = book.title
-    author.innerHTML = book.author
-    pages.innerHTML = book.pages
-    isRead.innerHTML = book.isRead
-}
-
 const init = () => {
     let currentBooks = JSON.parse(localStorage.getItem("library"))
 
@@ -53,13 +32,55 @@ const init = () => {
     }
 }
 
-init()
+const build = (elements) => {
+    return new Book(
+        getRandomId(),
+        elements.title.value,
+        elements.author.value,
+        elements.pages.value,
+        elements.is_read.checked
+    )
+}
+
+const createRemoveButtonElement = (book, row) => {
+    var removeButton = document.createElement('input')
+    removeButton.type = "button"
+    removeButton.className = "btn btn-danger"
+    removeButton.value = "remove"
+    removeButton.addEventListener("click", () => {
+        library = library.filter(x => x.id !== book.id)
+        localStorage.setItem("library", JSON.stringify(library))
+        row.remove()
+    })
+    return removeButton
+}
+
+const add = (book) => {
+    var row = table.insertRow()
+    row.id = book.id
+
+    var title = row.insertCell(0)
+    var author = row.insertCell(1)
+    var pages = row.insertCell(2)
+    var isRead = row.insertCell(3)
+    var removeButtonCell = row.insertCell(4)
+
+    title.innerHTML = book.title
+    author.innerHTML = book.author
+    pages.innerHTML = book.pages
+    isRead.innerHTML = book.isRead
+
+    removeButtonCell.appendChild(createRemoveButtonElement(book, row))
+}
 
 class Book {
-    constructor(title, author, pages, isRead = false) {
+    constructor(id, title, author, pages, isRead = false) {
+        this.id = id
         this.title = title
         this.author = author
         this.pages = pages
         this.isRead = isRead
     }
 }
+
+init()
